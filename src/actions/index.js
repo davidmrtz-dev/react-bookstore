a9f8acf0b6df8dd1f695ae01a7a4df8980fae7fb
David Elì <davidelimg1@gmail.com>
2020-08-25 12:32:04 -0500

    changed default props for new book on create book action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const createBook = (
   book = {
     id: Math.floor(Math.random() * 1000),
-    title: 'New Book',
-    category: null,
+    title: '',
+    category: '',
   },
 ) => ({
   type: CREATE_BOOK,
@@ -15,4 +16,9 @@ export const createBook = (
 export const removeBook = id => ({
   type: REMOVE_BOOK,
   id,
+});
+
+export const changeFilter = filter => ({
+  type: CHANGE_FILTER,
+  filter,
 });
