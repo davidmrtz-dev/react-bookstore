93878bb5a336df132ef3ee44a0e46670081d7c24
David Elì <davidelimg1@gmail.com>
2020-08-25 15:10:29 -0500

    Added container to boxes for good displaying

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 
 const Book = ({ book, handleRemoveBook }) => (
   <tr>
-    <td>{book.id}</td>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td>
-      <button type="button" onClick={handleRemoveBook}>
-        Remove
-      </button>
+    <td className="book-info">
+      <div className="id">{book.id}</div>
+      <div className="title">{book.title}</div>
+      <div className="category">{book.category}</div>
+      <div className="remove">
+        <button type="button" onClick={handleRemoveBook}>
+          Remove
+        </button>
+      </div>
     </td>
   </tr>
 );
