b433a0eb0df85f93dcf298a9d495ccc1f2c56032
David Elì <davidelimg1@gmail.com>
2020-08-25 12:24:41 -0500

    Added validation to fiels, also reset te input values after creating a Book

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
@@ -20,6 +21,7 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const BooksForm = ({ createBook }) => {
+  const [errors, setErrors] = useState({});
   const [book, setBook] = useState({
     id: Math.floor(Math.random() * 1000),
     title: '',
@@ -33,35 +35,69 @@ const BooksForm = ({ createBook }) => {
     });
   };
 
+  const formIsValid = () => {
+    const _errors = {};
+
+    if (!book.title) _errors.title = 'Title is required';
+    if (!book.category) _errors.category = 'Category is required';
+
+    setErrors(_errors);
+    return Object.keys(_errors).length === 0;
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
+    if (!formIsValid()) return;
+
     createBook({ id: book.id, title: book.title, category: book.category });
+    book.title = '';
+    book.category = '';
   };
+
+  let wrapperClass = 'form-group';
+  if (errors.length > 0) {
+    wrapperClass += 'has-error';
+  }
 
   return (
     <>
-      <label htmlFor="title">Title</label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        value={book.title}
-        onChange={handleChange}
-      />
-      <label htmlFor="category">Category</label>
-      <input
-        name="category"
-        list="category-list"
-        value={book.category || ''}
-        onChange={handleChange}
-      />
-      <datalist id="category-list">
-        {categories.map(cat => (
-          <option key={cat} value={cat}>
-            {cat}
-          </option>
-        ))}
-      </datalist>
+      <div className={wrapperClass}>
+        <label htmlFor="title">Title</label>
+        <div className="field">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={book.title}
+            onChange={handleChange}
+          />
+        </div>
+        {errors.title && (
+          <div className="alert alert-danger">{errors.title}</div>
+        )}
+      </div>
+
+      <div className={wrapperClass}>
+        <label htmlFor="category">Category</label>
+        <div className="field">
+          <input
+            name="category"
+            list="category-list"
+            value={book.category || ''}
+            onChange={handleChange}
+          />
+          <datalist id="category-list">
+            {categories.map(cat => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </datalist>
+        </div>
+        {errors.category && (
+          <div className="alert alert-danger">{errors.category}</div>
+        )}
+      </div>
       <button type="submit" onClick={handleSubmit}>
         Add book
       </button>
