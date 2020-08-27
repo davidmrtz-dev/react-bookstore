7716303884f67ffb528d860072a3bdfa8691905c
David Elì <davidelimg1@gmail.com>
2020-08-26 20:00:58 -0500

    Applying reducers composition to add, remove and update a book with the id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const UPDATE_BOOK_PROGRESS = 'UPDATE_BOOK_PROGRESS';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const createBook = book => ({
@@ -10,6 +11,12 @@ export const createBook = book => ({
 export const removeBook = id => ({
   type: REMOVE_BOOK,
   id,
+});
+
+export const updateBookProgress = (id, progress) => ({
+  type: UPDATE_BOOK_PROGRESS,
+  id,
+  progress,
 });
 
 export const changeFilter = filter => ({
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,9 +1,26 @@
-import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
+import {
+  CREATE_BOOK,
+  REMOVE_BOOK,
+  UPDATE_BOOK_PROGRESS,
+} from '../actions/index';
+
+const book = (state, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return {
+        ...action.book,
+      };
+    default:
+      return state;
+  }
+};
 
 const books = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return [...state, action.book];
+      return [...state, book(undefined, action)];
+    case UPDATE_BOOK_PROGRESS:
+      return {};
     case REMOVE_BOOK:
       return state.filter(book => book.id !== action.id);
     default:
